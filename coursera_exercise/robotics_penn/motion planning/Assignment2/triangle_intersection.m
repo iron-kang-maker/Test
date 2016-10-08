330d29039ee6370779dfc8fc43a5581d42cd1560
iron_kang <iron.kcj@gmail.com>
2016-10-08 21:12:38 +0800

    modify triangle_inersetion

--- coursera_exercise/robotics_penn/motion planning/Assignment2/triangle_intersection.m
+++ coursera_exercise/robotics_penn/motion planning/Assignment2/triangle_intersection.m
@@ -8,47 +8,25 @@ n = size(P1, 1);
 flag = true;
 
 for k = 1:n
-    A = [P1(k,1) 1; P1(mod(k,3)+1,1) 1];
-    b = [P1(k,2); P1(mod(k,3)+1,2)];
+    p1 = P1(k,:);
+    p2 = P1(mod(k,3)+1,:);
+    p = P1(mod(k+1,3)+1, :);
     
     sigP1 = 0;
     sigP2 = 0;
     
-    rk = rank(A);
-    if (rk ~= 1)
-        m = A\b;
-    
-        p1 = [P1(mod(k+1, 3)+1, 1) P1(mod(k+1, 3)+1, 2)];
-    
-        
-        if p1(2) < (m(1)*p1(1) + m(2))
-            sigP1 = sigP1 - 1;
-        else
-            sigP1 = sigP1 + 1;
-        end
-    
-        
-        for l = 1:n
-            if P2(l,2) > (m(1)*P2(l,1) + m(2))
-                sigP2 = sigP2 + 1;
-            elseif P2(l,2) < (m(1)*P2(l,1) + m(2))
-                sigP2 = sigP2 - 1;
-            end
-        end
+    if line_function(p1, p2, p) > 0
+        sigP1 = 1;
     else
-        p1 = P1(mod(k+1, 3)+1, 1);
-        if p1 > P1(k,1)
-            sigP1 = 1;
+        sigP1 = -1;
+    end
+    
+    for l = 1:n
+        p = P2(l,:);
+        if line_function(p1, p2, p) > 0
+            sigP2 = sigP2 + 1;
         else
-            sigP1 = -1;
-        end
-        
-        for l = 1:n
-            if P2(l,1) > P1(k,1)
-                sigP2 = sigP2 + 1;
-            elseif P2(l,1) < P1(k,1)
-                sigP2 = sigP2 - 1;
-            end
+            sigP2 = sigP2 -1;
         end
     end
     
@@ -60,47 +38,25 @@ for k = 1:n
 end
 
 for k = 1:n
-    A = [P2(k,1) 2; P2(mod(k,3)+1,1) 1];
-    b = [P2(k,2); P2(mod(k,3)+1,2)];
+    p1 = P2(k,:);
+    p2 = P2(mod(k,3)+1,:);
+    p = P2(mod(k+1,3)+1, :);
     
     sigP1 = 0;
     sigP2 = 0;
     
-    rk = rank(A);
-    if (rk ~= 1)
-        m = A\b;
-    
-        p2 = [P2(mod(k+1, 3)+1, 1) P2(mod(k+1, 3)+1, 2)];
-    
-        
-        if p2(2) < (m(1)*p2(1) + m(2))
-            sigP2 = sigP2 - 1;
-        else
-            sigP2 = sigP2 + 1;
-        end
-    
-        
-        for l = 1:n
-            if P1(l,2) > (m(1)*P1(l,1) + m(2))
-                sigP1 = sigP1 + 1;
-            elseif P1(l,2) < (m(1)*P1(l,1) + m(2))
-                sigP1 = sigP1 - 1;
-            end
-        end
+    if line_function(p1, p2, p) > 0
+        sigP1 = 1;
     else
-        p2 = P2(mod(k+1, 3)+1, 1);
-        if p2 > P2(k,1)
-            sigP2 = 1;
+        sigP1 = -1;
+    end
+    
+    for l = 1:n
+        p = P1(l,:);
+        if line_function(p1, p2, p) > 0
+            sigP2 = sigP2 + 1;
         else
-            sigP2 = -1;
-        end
-        
-        for l = 1:n
-            if P1(l,1) > P2(k,1)
-                sigP1 = sigP1 + 1;
-            elseif P1(l,1) < P2(k,1)
-                sigP1 = sigP1 - 1;
-            end
+            sigP2 = sigP2 -1;
         end
     end
     
